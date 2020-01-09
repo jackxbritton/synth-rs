92733510d29fd3b138e996eed98e07410f72165b
= <=>
2020-01-09 18:06:15 -0500

    Finish envelopes and oscillators, and it's only a little gross.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ extern crate crossbeam_channel;
 extern crate jack;
 use crossbeam_channel::{bounded, unbounded};
 use notify::{RecommendedWatcher, RecursiveMode, Watcher};
-use std::cell::RefCell;
 use std::collections::HashMap;
 use std::error::Error;
 use std::f64::consts::PI;
@@ -11,16 +10,7 @@ use std::io;
 use std::path::Path;
 use std::time::Duration;
 
-enum Module {
-    Constant(f64),
-    Envelope(EnvelopeModule),
-    SineOscillator(OscillatorModule),
-    SawtoothOscillator(OscillatorModule),
-    SquareOscillator(OscillatorModule),
-
-    Variable(String),
-}
-
+#[derive(Clone, Copy)]
 enum ModuleHandle {
     Constant(f64),
     Envelope(usize),
@@ -53,10 +43,10 @@ struct Config {
     on: bool,
     time_since_toggle: f64,
 
-    envelopes: RefCell<Vec<EnvelopeModule>>,
-    sine_oscillators: RefCell<Vec<OscillatorModule>>,
-    sawtooth_oscillators: RefCell<Vec<OscillatorModule>>,
-    square_oscillators: RefCell<Vec<OscillatorModule>>,
+    envelopes: Vec<EnvelopeModule>,
+    sine_oscillators: Vec<OscillatorModule>,
+    sawtooth_oscillators: Vec<OscillatorModule>,
+    square_oscillators: Vec<OscillatorModule>,
 
     out: ModuleHandle,
 }
@@ -69,10 +59,10 @@ impl Config {
             frequency: 0.0,
             on: false,
             time_since_toggle: 0.0,
-            envelopes: RefCell::new(Vec::new()),
-            sine_oscillators: RefCell::new(Vec::new()),
-            sawtooth_oscillators: RefCell::new(Vec::new()),
-            square_oscillators: RefCell::new(Vec::new()),
+            envelopes: Vec::new(),
+            sine_oscillators: Vec::new(),
+            sawtooth_oscillators: Vec::new(),
+            square_oscillators: Vec::new(),
             out: ModuleHandle::Constant(0.0),
         };
 
@@ -104,6 +94,10 @@ impl Config {
         value: &serde_json::Value,
     ) -> Result<ModuleHandle, Box<dyn Error>> {
         match value {
+            serde_json::Value::Number(number) => {
+                let number = number.as_f64().ok_or(format!("failed to parse '{}' as f64", number))?;
+                Ok(ModuleHandle::Constant(number))
+            }
             serde_json::Value::String(key) => {
                 let variable = variables
                     .get(key)
@@ -111,7 +105,27 @@ impl Config {
                 Ok(*variable)
             }
             serde_json::Value::Object(object) => {
-                if let (Some(shape), Some(amplitude), Some(frequency)) = (
+                if let (Some(attack), Some(decay), Some(sustain), Some(release)) = (
+                    object.get("attack"),
+                    object.get("decay"),
+                    object.get("sustain"),
+                    object.get("release"),
+                ) {
+                    let (attack, decay, sustain, release) = (
+                        self.add_json(variables, attack)?,
+                        self.add_json(variables, decay)?,
+                        self.add_json(variables, sustain)?,
+                        self.add_json(variables, release)?,
+                    );
+                    self.envelopes.push(EnvelopeModule {
+                        attack: attack,
+                        decay: decay,
+                        sustain: sustain,
+                        release: release,
+                        out: 0.0,
+                    });
+                    Ok(ModuleHandle::Envelope(self.envelopes.len() - 1))
+                } else if let (Some(shape), Some(amplitude), Some(frequency)) = (
                     object.get("shape"),
                     object.get("amplitude"),
                     object.get("frequency"),
@@ -120,15 +134,39 @@ impl Config {
                         serde_json::Value::String(shape) => Ok(shape),
                         _ => Err("key 'shape' in oscillator must be of type string"),
                     }?;
+                    let (amplitude, frequency) = (
+                        self.add_json(variables, amplitude)?,
+                        self.add_json(variables, frequency)?,
+                    );
                     if shape == "sine" {
-                        self.sine_oscillators.borrow_mut().push(OscillatorModule {
-                            amplitude: self.add_json(variables, amplitude)?,
-                            frequency: self.add_json(variables, frequency)?,
+                        self.sine_oscillators.push(OscillatorModule {
+                            amplitude: amplitude,
+                            frequency: frequency,
                             phase: 0.0,
                             out: 0.0,
                         });
                         Ok(ModuleHandle::SineOscillator(
-                            self.sine_oscillators.borrow().len() - 1,
+                            self.sine_oscillators.len() - 1,
+                        ))
+                    } else if shape == "sawtooth" {
+                        self.sawtooth_oscillators.push(OscillatorModule {
+                            amplitude: amplitude,
+                            frequency: frequency,
+                            phase: 0.0,
+                            out: 0.0,
+                        });
+                        Ok(ModuleHandle::SawtoothOscillator(
+                            self.sawtooth_oscillators.len() - 1,
+                        ))
+                    } else if shape == "square" {
+                        self.square_oscillators.push(OscillatorModule {
+                            amplitude: amplitude,
+                            frequency: frequency,
+                            phase: 0.0,
+                            out: 0.0,
+                        });
+                        Ok(ModuleHandle::SquareOscillator(
+                            self.square_oscillators.len() - 1,
                         ))
                     } else {
                         Err("key 'shape' in oscillator must be 'sine', 'sawtooth', or 'square'")?
@@ -136,17 +174,8 @@ impl Config {
                 } else {
                     Err(format!("failed to parse JSON object {:?}", object))?
                 }
-                /*
-                let variables = match value
-                    .get("shape")
-                    .ok_or("top-level key 'variables' not found")?
-                {
-                    serde_json::Value::Object(variables) => Ok(variables),
-                    _ => Err("top-level key 'variables' must be an object"),
-                }?;
-                */
             }
-            _ => Err(format!("failed to parse JSON value {}", value))?,
+            _ => Err(format!("failed to parse JSON value '{}'", value))?,
         }
     }
 
@@ -185,12 +214,10 @@ impl Config {
     fn get_module_output(&self, handle: ModuleHandle) -> f64 {
         match handle {
             ModuleHandle::Constant(value) => value,
-            ModuleHandle::Envelope(index) => self.envelopes.borrow()[index].out,
-            ModuleHandle::SineOscillator(index) => self.sine_oscillators.borrow()[index].out,
-            ModuleHandle::SawtoothOscillator(index) => {
-                self.sawtooth_oscillators.borrow()[index].out
-            }
-            ModuleHandle::SquareOscillator(index) => self.square_oscillators.borrow()[index].out,
+            ModuleHandle::Envelope(index) => self.envelopes[index].out,
+            ModuleHandle::SineOscillator(index) => self.sine_oscillators[index].out,
+            ModuleHandle::SawtoothOscillator(index) => self.sawtooth_oscillators[index].out,
+            ModuleHandle::SquareOscillator(index) => self.square_oscillators[index].out,
         }
     }
 
@@ -202,13 +229,6 @@ impl Config {
 
         // Update modules.
 
-        for osc in self.sine_oscillators.borrow_mut().iter_mut() {
-            let a = self.get_module_output(osc.amplitude);
-            let f = self.get_module_output(osc.frequency);
-            osc.phase = (osc.phase + f * dt) % 1.0;
-            osc.out = a * (osc.phase * 2.0 * PI).sin();
-        }
-        /*
         for index in 0..self.sine_oscillators.len() {
             let osc = &self.sine_oscillators[index];
             let a = self.get_module_output(osc.amplitude);
@@ -233,50 +253,48 @@ impl Config {
             mut_osc.phase = (mut_osc.phase + f * dt) % 1.0;
             mut_osc.out = a * (if mut_osc.phase < 0.5 { -1.0 } else { 1.0 });
         }
-        */
+        for index in 0..self.envelopes.len() {
+            let env = &self.envelopes[index];
+            let t = self.time_since_toggle;
+            let (a, d, s, r) = (
+                self.get_module_output(env.attack),
+                self.get_module_output(env.decay),
+                self.get_module_output(env.sustain),
+                self.get_module_output(env.release),
+            );
+            let mut_env = &mut self.envelopes[index];
+            mut_env.out = if self.on {
+                if t < a {
+                    if a == 0.0 {
+                        1.0
+                    } else {
+                        t / a
+                    }
+                } else if t < a + d {
+                    if d == 0.0 {
+                        s
+                    } else {
+                        s + (1.0 - s) * (1.0 - (t - a) / d)
+                    }
+                } else {
+                    s
+                }
+            } else {
+                if t < r {
+                    0.0
+                } else {
+                    if r == 0.0 {
+                        0.0
+                    } else {
+                        s * t / r
+                    }
+                }
+            };
+        }
 
         self.get_module_output(self.out)
     }
 }
-
-// impl Operator for EnvelopeOperator {
-//     fn update(&mut self, config: &Config) {
-//         let t = config.time_since_toggle;
-//         let (a, d, s, r) = (
-//             config.get(&self.attack),
-//             config.get(&self.decay),
-//             config.get(&self.sustain),
-//             config.get(&self.release),
-//         );
-//         self.out = if config.on {
-//             if t < a {
-//                 if a == 0.0 {
-//                     1.0
-//                 } else {
-//                     t / a
-//                 }
-//             } else if t < a + d {
-//                 if d == 0.0 {
-//                     s
-//                 } else {
-//                     s + (1.0 - s) * (1.0 - (t - a) / d)
-//                 }
-//             } else {
-//                 s
-//             }
-//         } else {
-//             if t < r {
-//                 0.0
-//             } else {
-//                 if r == 0.0 {
-//                     0.0
-//                 } else {
-//                     s * t / r
-//                 }
-//             }
-//         };
-//     }
-// }
 
 fn read_config_from_file<P: AsRef<Path>>(path: P) -> Result<Config, Box<dyn Error>> {
     let file = fs::File::open(path)?;
@@ -292,7 +310,7 @@ fn do_paths_refer_to_same_file<P: AsRef<Path>>(a: P, b: P) -> io::Result<bool> {
 fn main() -> Result<(), Box<dyn Error>> {
     // Read config.
     let path = Path::new("test.json");
-    let config = read_config_from_file(path)?;
+    let mut config = read_config_from_file(path)?;
 
     // Open a client and register a port.
     let (client, _status) = jack::Client::new(
